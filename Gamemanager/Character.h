f752b05d01cf17af9023385cb1654fbba5d23d5a
gukin <tlaghr12@naver.com>
2025-06-20 16:30:07 +0900

    Chararcter .cpp .h Item .h
    변수 선언까지만 해놓았습니다?

--- Gamemanager/Character.h
+++ Gamemanager/Character.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "Item.h"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Character {
+public:
+
+private:
+	string Name;
+	int Level;
+	int MaxHp;
+	int CurrentHp;
+	int Attack;
+	int Exp;
+	int Gold;
+	vector<Item>inventory;
+
+};
