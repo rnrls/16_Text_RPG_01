2aca91a16e381db91cfa11bf4024a3bfcb120bdb
YAKWANGTAN <mkh1517@gmail.com>
2025-06-20 17:39:58 +0900

    GenerateMonster
    GenerateMonster

--- Text_Game/Gamemanager.cpp
+++ Text_Game/Gamemanager.cpp
@@ -5,6 +5,14 @@
 
 using namespace std;
 
+Monster* generateMonster(int level) {
+    vector<string> names = { "Goblin", "Orc", "Troll", "Slime" };
+    int idx = rand() % names.size();
+    int baseHealth = 100 + (level * 10);
+    int baseAttack = 10 + (level * 2);
+
+    return new Monster(names[idx], baseHealth, baseAttack);
+
 void Gamemanager::displayInventory(Character* player)
 {
     // 인벤토리 목록
