f28c715e1c1f26d643d15ff45bd726628a6d1579
nayoungjin39 <211291869+nayoungjin39@users.noreply.github.com>
2025-06-23 12:21:25 +0900

    Update Start.cpp

--- Text_Game/Start.cpp
+++ Text_Game/Start.cpp
@@ -2,13 +2,11 @@
 
 using namespace std;
 
-enum start_choice
+enum MM_CHOICE
 {
-	VIEW_STATS = 1,
-	TOWN = 2,
-	BEGINNER_FIELD = 3,
-	DUNGEON = 4,
-	EXIT = 5
+	START_GAME = 1,
+	OPTION = 2,
+	EXIT = 3
 };
 
 void StartGameLoop();
@@ -16,22 +14,20 @@ void StartGameLoop();
 int main()
 {
 	cout << "=====================================" << endl;
-	cout << "          <16TEAM_Text RPG>          " << endl;
+	cout << "              <Text RPG>             " << endl;
 	cout << "=====================================" << endl;
 
 	system("pause");
 
+	// Loop
 	while (true)
 	{
 		system("cls");
 
-		cout << "==============[Text RPG)=============" << endl;
-		cout << "         1. 캐릭터 스텟 보기         " << endl;
-		cout << "         2. 마을로 가기              " << endl;
-		cout << "         3. 초보자 사냥터            " << endl;
-		cout << "         4. 던전                     " << endl;
-		cout << "         5. 게임 종료                " << endl; 
-		cout << "=====================================" << endl;
+		cout << "================Main menu================" << endl;
+		cout << "1. 게임 시작" << endl;
+		cout << "2. 나가기" << endl;
+		cout << "=========================================" << endl;
 
 		int choice;
 		cin >> choice;
@@ -43,42 +39,24 @@ int main()
 			continue;
 		}
 
-		switch (static_cast<start_choice>(choice)) {
-		case VIEW_STATS:
-			hero->displayStatus();
+		switch (static_cast<MM_CHOICE>(choice))
+		{
+		case MM_CHOICE::START_GAME:
+			StartGameLoop();
 			break;
-
-		case TOWN:
-			cout << ">> 마을에서 체력을 회복합니다." << endl;
-			hero->health = hero->maxHealth;
+		case MM_CHOICE::OPTION:
+			// OPTION
 			break;
-
-		case BEGINNER_FIELD:
-			cout << ">> 초보자 사냥터로 출발!" << endl;
-			GameManager::battle(hero);
+		case MM_CHOICE::EXIT:
 			break;
-
-		case DUNGEON:
-			cout << ">> 던전으로 이동합니다!" << endl;
-			GameManager::battle(hero);
-			break;
-
-		case EXIT: 
-			cout << "게임을 종료합니다. 감사합니다!" << endl;
-			return; 
-
 		default:
-			cout << ">> 잘못된 입력입니다. 다시 선택하세요." << endl;
-		}
-
-		if (hero->health <= 0) {
-			cout << hero->name << "가 사망했습니다. 게임 오버!" << endl;
 			break;
 		}
+	}
 
 	system("cls");
 
-	cout << "게임 종료" << endl;
+	cout << "게임 종료..." << endl;
 	cout << "=====================================" << endl;
 	system("pause");
 	return 0;
