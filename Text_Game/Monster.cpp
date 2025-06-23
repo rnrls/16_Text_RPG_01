aaf457a9ef75b5031e7145e84af0e9fed91c0aa0
Hyeongcheol <gudcjf6543@gmail.com>
2025-06-23 17:29:11 +0900

    몬스터 스텟 변경

--- Text_Game/Monster.cpp
+++ Text_Game/Monster.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-Irongorem::Irongorem(int level) : Monster("아이언 골렘", 20 * level, 5 * level) {}
+Irongorem::Irongorem(int level) : Monster("아이언 골렘", 7 * level, 5 * level) {}
 
 
-Lich::Lich(int level) : Monster("리치", 30 * level, 6 * level) {}
+Lich::Lich(int level) : Monster("리치", 8 * level, 6 * level) {}
 
 
-Griffon::Griffon(int level) : Monster("그리폰", 40 * level, 7 * level) {}
+Griffon::Griffon(int level) : Monster("그리폰", 9 * level, 7 * level) {}
 
 
-Nightmare::Nightmare(int level) : Monster("나이트 메어", 60* level, 9 * level) {}
+Nightmare::Nightmare(int level) : Monster("나이트 메어", 10 * level, 9 * level) {}
 
 
 
