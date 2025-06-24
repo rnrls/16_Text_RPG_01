dcd1e28a9cf5584344681d19245fac7aaa20c7b6
Hyeongcheol <gudcjf6543@gmail.com>
2025-06-24 16:58:42 +0900

    Revert "name"
    This reverts commit 627011f813111b1abc2de495e8daacd048e34701.

--- Text_Game/Gamemanager.cpp
+++ Text_Game/Gamemanager.cpp
@@ -82,8 +82,6 @@ void Gamemanager::Battle(Character* player, Monster* monster) {
 
         if (player->isDead()) {
             cout << player->getName() << "이(가) 쓰러졌다..." << endl;
-            cout << "\n=== [게임 오버] ===\n";
-            system("pause");
             break;
         }
 
