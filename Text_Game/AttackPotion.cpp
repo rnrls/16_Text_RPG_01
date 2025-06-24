1576401e6425e68eb7d1fc666ade41d14bd9726f
geonppang0323 <213411894+geonppang0323@users.noreply.github.com>
2025-06-24 21:22:03 +0900

    수정

--- Text_Game/AttackPotion.cpp
+++ Text_Game/AttackPotion.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 void AttackPotion::use(Character* character) {
-    character->AddAttack(30);
+    character->AddAttack(20);
     std::cout << "[아이템 사용] 공격력이 20 증가했습니다!" << std::endl;
 }
 /**/
